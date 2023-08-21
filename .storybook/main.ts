f63b9169c2996566f289d8ed8422322a5811e310
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2023-08-21 11:47:41 +0200

    Update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,21 @@
+/*
+   Copyright 2023 Betim Beja and Shko Online LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 import type { StorybookConfig } from "@storybook/html-webpack5";
+const webpack = require('webpack');
 
 const config: StorybookConfig = {
   stories: [
@@ -16,6 +33,22 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async config => {
+    config.devtool = false;
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = config.resolve.fallback || {};
+    config.resolve.fallback['fs'] = false;
+    config.plugins = config.plugins || [];
+    config.plugins.push(new webpack.SourceMapDevToolPlugin({
+      append: '\n//# sourceMappingURL=[url]',
+      fileContext: './',
+      filename: '[file].map'
+    }));
+    config.plugins.push(new webpack.ProvidePlugin({
+      Buffer: ['buffer/', 'Buffer']
+    }));
+    return config;
   },
 };
 export default config;
